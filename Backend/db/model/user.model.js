10af203c4cbd75a1ee4c49a0c6572fee0ea947a6
Mubeen006 <cmubeen599@gmai.com>
2024-12-05 21:10:02 +0500

    Create mongodb  connection and usermodel

---
+++ Backend/db/model/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+// create a user model to store user date in db
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const user= mongoose.model("user",userSchema);
+export default user;
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,14 @@
 import express from "express";
 const app= express();
+// create a mongodb connection
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+ mongoose.connect(process.env.Mongodb).then(() => {
+    console.log("Connected to MongoDB");
+ }).catch((error) => {
+    console.log(error);
+ });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
